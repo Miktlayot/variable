be0b2d21b919530224aba7dcb194d80316c64e0e
Miktlayot <aleksandrmaslov1187@gmail.com>
2024-12-15 15:17:41 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+how_many_homeworks = 12
+how_many_times = 1.5
+name_of_profile = 'Python'
+time_to_one_homework = (how_many_times / how_many_homeworks)
+print(name_of_profile, how_many_homeworks, how_many_times, time_to_one_homework)
